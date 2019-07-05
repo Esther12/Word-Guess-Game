69f07bf336414d0e683beac4e26cba51a116f880
Yating(Esther) Su <yaringsu@foxmail.com>
2019-07-05 17:13:21 -0400

    about finished ok to use, still have problem with delay

--- assets/javacript/game.js
+++ assets/javacript/game.js
@@ -1,69 +1,93 @@
-
-var guessedList = [];
-var answers = [];
-var i;
-var aword = [];
+var Guessed;
+var guessedList = [];//Screen
+var answers = [];//back one element of obj
+var answerWord;//back w:----
+var answerImg;//back url:#
+var screenWord = [];//Screen
+var countScore;//the score
+var countLife = 6;//the total life
 var obj = [
-    {w:"words0",url:"#"},
-    {w:"words1",url:"#"},
-    {w:"words2",url:"#"},
+    {w:"words0",url:"assets/images/stop watching video.jpg"},
+    {w:"words1",url:"assets/images/tidy.jpg"},
+    {w:"words2",url:"assets/images/travel-tourism.jpg"},
     {w:"words3",url:"#"},
     {w:"words4",url:"#"}];
-    i = obj.length;
-    var Guessed;
-    console.log(i);
-    
-    // for(var i = 0; i < obj.length; i++){
-        answers = obj[getRandomInt(i)].w;
 
-    // }
-     console.log(answers);
-    for(var a = 0; a<answers.length; a++){
-        aword[a] = '_';
-   }
-   document.getElementById("words").textContent = aword;
+    gameStart();
+    gameNext();
  // This function is run whenever the user presses a key.
- var found = aword.find(function(element) {
-    return element == "_";
-  });
+
  document.onkeyup = function(event) {
-     
+
     // Determines which key was pressed.
         Guessed = event.key;
-   // for(var i = 0; i < answers.length; i++){
-        // for(var a = 0; a<answers[i].length; a++){
-        //      aword += "_";
-        // }
+        
+
         console.log(Guessed);
-        var found = aword.find(function(element) {
-            return element == "_";
-          });
-        if(found == undefined || guessedList > (aword.length *2)){
-            console.log(i);
-            guessedList = [];
-            break;
-            console.log(guessedList);
-        }
-        for(var j = 0; j<answers.length; j++){
-            if(Guessed == answers[j]){
-                // aword.replace(aword[j], Guessed);
-                //aword[j] = Guessed;
-                aword.splice(j, 1, Guessed);
-                console.log(aword);
-                document.getElementById("words").textContent = aword;
+
+        scoreAndCount();
+
+        for(let j = 0; j < answerWord.length; j++){
+            if(Guessed == answerWord[j]){
+                screenWord.splice(j, 1, Guessed);
+                console.log(screenWord);
+                document.getElementById("words").textContent = screenWord;
             }
         }
         guessedList+= Guessed;
         document.getElementById("G-list").textContent = guessedList;
+        document.getElementById("rounds").textContent = "Life: " + countLife;
+        document.getElementById("marks").textContent = "Score: " + countScore;
         console.log(guessedList);
-
-       // }
        
     }
 
  
 
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * Math.floor(max));
-      }
-
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+function gameNext(){
+    guessedList =[];
+    answers = [];
+    screenWord = [];
+    answerWord = [];
+    answerImg = "";
+    answers = obj[getRandomInt(obj.length)];
+    answerWord = answers.w;
+    answerImg = answers.url;
+    console.log(answers);
+    for(let a = 0; a < answerWord.length; a++){
+            screenWord[a] = '_';
+    }
+    document.getElementById("words").textContent = screenWord;
+    document.getElementById("imgs").src = answerImg;
+} 
+function gameStart(){
+    gameNext();
+    countLife = 6;
+    countScore = 0;
+    
+}
+function scoreAndCount(){
+    let lengthTwo = screenWord.length*2;
+    let found = screenWord.find(function(element) {
+        return element == "_";
+      });
+      if(found == undefined){
+        guessedList = [];
+        gameNext();
+        countScore++;
+        console.log(guessedList);
+        alert("Nice Game!");
+    }else if(guessedList.length > lengthTwo){
+        countLife-- ;
+        guessedList = [];
+        if(countLife < 0){
+            gameStart();
+            alert("Bad Game! Better next time!");  
+        }else{
+            gameNext();
+        }
+    }
+}
