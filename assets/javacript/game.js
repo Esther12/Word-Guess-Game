ffb46ad7b0971fa703a16958bc6775b0c735a870
Yating(Esther) Su <yaringsu@foxmail.com>
2019-07-05 10:37:19 -0400

    deBug for the aword and fixed the loops

--- assets/javacript/game.js
+++ assets/javacript/game.js
@@ -1,41 +1,57 @@
 debugger
 var guessedList = [];
 var answers = [];
+var i;
 var aword ="";
-var obj = [{w:"words",url:"#"},{w:"words",url:"#"},{w:"words",url:"#"},
-    {w:"words",url:"#"},{w:"words",url:"#"}];
-    for(var i = 0; i < obj.length; i++){
-        answers[i] = obj[i].w;
-    }
-    // console.log(answers);
+var obj = [
+    {w:"words0",url:"#"},
+    {w:"words1",url:"#"},
+    {w:"words2",url:"#"},
+    {w:"words3",url:"#"},
+    {w:"words4",url:"#"}];
+    i = obj.length;
+    console.log(i);
+    
+    // for(var i = 0; i < obj.length; i++){
+        answers = obj[getRandomInt(i)].w;
 
+    // }
+     console.log(answers);
+    for(var a = 0; a<answers.length; a++){
+        aword += "_";
+   }
+   document.getElementById("words").textContent = aword;
  // This function is run whenever the user presses a key.
  document.onkeyup = function(event) {
 
     // Determines which key was pressed.
     var Guessed = event.key;
-
-    document.getElementById("words").textContent = aword;
-    document.getElementById("G-list").textContent = guessedList;
-    for(var i = 0; i < answers.length; i++){
-        for(var a = 0; a<answers[i].length; a++){
-             aword += "_";
-        }
+   // for(var i = 0; i < answers.length; i++){
+        // for(var a = 0; a<answers[i].length; a++){
+        //      aword += "_";
+        // }
+        console.log(aword);
         if(aword.search("_")== -1 || guessedList > (aword.length *2)){
-            i++;
+            console.log(i);
             guessedList = [];
+            console.log(guessedList);
         }
-        for(var j = 0; j<answers[i].length; j++){
-            if(Guessed == answers[i][j]){
+        for(var j = 0; j<answers.length; j++){
+            if(Guessed == answers[j]){
                 aword[j] = Guessed;
-                guessedList+= Guessed;
-            }else{
-                guessedList += Guessed;
+                console.log(aword);
             }
         }
+        guessedList+= Guessed;
+        console.log(guessedList);
 
-        }
+       // }
+       document.getElementById("G-list").textContent = guessedList;
     }
 
+ 
 
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+      }
 
